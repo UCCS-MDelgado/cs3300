3ad508e66bcd11d39e6d0500e562b9d391f57853
root <mdelgad3@uccs.edu>
2022-11-26 00:51:29 +0000

    Pushing to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
 
-  config.action_mailer.default_url_options = {"https://intense-tundra-82151.herokuapp.com"}
+  config.action_mailer.default_url_options = {'https://intense-tundra-82151.herokuapp.com'}
 end
