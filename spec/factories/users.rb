32935a6db061eb51f9e623f00d5889f520fa1f40
root <mdelgad3@uccs.edu>
2022-11-27 09:11:39 +0000

    Set Up Factory Bot User Model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :user do
-      email { 'john@email_provider.com' }
+      email { "john@email_provider.com" }
       password { 'password' }
       password_confirmation { 'password' }
     end
