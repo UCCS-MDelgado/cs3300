feb20da7ac4caaecafb74a39ed9373f04cc1f575
root <mdelgad3@uccs.edu>
2022-11-27 10:46:55 +0000

    Fixed Integration Specs for Title, Description, and Project Removal

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -26,6 +26,8 @@ RSpec.feature "Projects", type: :feature do
   context "Update project" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
     before(:each) do
+      testUser = FactoryBot.create(:user)
+      sign_in testUser
       visit edit_project_path(project)
     end
 
@@ -49,6 +51,8 @@ RSpec.feature "Projects", type: :feature do
   context "Remove existing project" do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
      scenario "remove project" do
+    testUser = FactoryBot.create(:user)
+    sign_in testUser
      visit project_path(project)
      click_button "Destroy this project"
      expect(page).to have_content("Project was successfully destroyed")
